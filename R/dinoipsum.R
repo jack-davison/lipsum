5a2bee59d33d086a6bd5ef98b93a729cb4fe0251
Jack Davison <davison.jack.jd@gmail.com>
2023-08-30 12:47:44 +0100

    add dinoipsum

---
+++ R/dinoipsum.R
@@ -0,0 +1,25 @@
+#' Randomly Generate Dinosaur Names using the Dinoipsum API
+#'
+#' Generates paragraphs of dinosaur names using the
+#'
+#' @param n_paras Integer. The number of paragraphs to generate. Defaults to 10.
+#' @param n_words Integer. The number of words to generate per paragraph.
+#'   Defaults to 30.
+#'
+#' @family whimsical generators
+#' @source <https://dinoipsum.com/>
+#'
+#' @export
+dinoipsum <- function(n_paras = 10L,
+                      n_words = 30L) {
+  url <-
+    paste0(
+      "https://dinoipsum.com/api/?format=text&paragraphs=",
+      as.integer(n_paras),
+      "&words=",
+      as.integer(n_words)
+    )
+
+  raw <- httr::GET(url)
+  rawToChar(raw$content)
+}
