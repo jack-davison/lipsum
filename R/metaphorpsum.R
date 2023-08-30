7f231029ac676b498e99b6553b4aab5b2c2b662f
Jack Davison <davison.jack.jd@gmail.com>
2023-08-30 09:54:38 +0100

    add metaphorpsum

---
+++ R/metaphorpsum.R
@@ -0,0 +1,31 @@
+#' Obtain wise, pedantic metaphors using the Metaphorpsum API
+#'
+#' Generates place holder text of nonsensical metaphors.
+#'
+#' @param n_paras Integer. The number of paragraphs to generate. Defaults to 2.
+#' @param n_sentences Integer. The number of sentences to generate per
+#'   paragraph. Defaults to 4.
+#' @param p `r loripsum_arg("wraps paragraphs in paragraph tags", "FALSE")`
+#'
+#' @family whimsical generators
+#' @source <http://metaphorpsum.com/>
+#'
+#' @export
+metaphorpsum <- function(n_paras = 2L,
+                         n_sentences = 4L,
+                         p = FALSE) {
+  url <-
+    paste0(
+      "metaphorpsum.com/paragraphs/",
+      as.integer(n_paras),
+      "/",
+      as.integer(n_sentences)
+    )
+
+  if (p) {
+    url <- paste0(url, "?p=true")
+  }
+
+  raw <- httr::GET(url)
+  rawToChar(raw$content)
+}
